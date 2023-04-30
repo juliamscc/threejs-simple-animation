25e316f929d73a63ded3988221522319712da262
Danilo Borges da Silva <daniloborges.cg@gmail.com>
2023-04-29 22:22:44 -0300

    adicionando tick

--- src/script.js
+++ src/script.js
@@ -29,3 +29,8 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
+
+//Animations
+const tick = () => {
+    console.log('tick')
+}
